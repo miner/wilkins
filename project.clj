5b12fce34f13404960d95ff03208d19801f82424
Steve Miner <steveminer@gmail.com>
2013-01-02 10:02:09 -0500

    desc

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.velisco/wilkins "0.1.0"
-  :description "Experimental conditional compilation using tagged literals"
+(defproject com.velisco/wilkins "0.1.1"
+  :description "Experimental lib for Clojure conditional reader using tagged literals"
   :url "http://github.com/miner/wilkins"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
