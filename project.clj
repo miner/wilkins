ab026aa23bafa9d12b1dd26531e677f902dd2bd3
Steve Miner <steveminer@gmail.com>
2014-02-25 15:08:52 -0500

    prep version 0.1.8

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.velisco/wilkins "0.2.0-SNAPSHOT"
+(defproject com.velisco/wilkins "0.1.8-SNAPSHOT"
   :description "Experimental lib for Clojure conditional reader using tagged literals"
   :url "http://github.com/miner/wilkins"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0-beta1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
 
   ;; for Clojure master-SNAPSHOT releases
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
