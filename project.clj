7929b3459ad366751873985b2326cf53c01d4c2c
Steve Miner <steveminer@gmail.com>
2014-05-01 18:37:39 -0400

    update for Clojure 1.6

--- project.clj
+++ project.clj
@@ -3,14 +3,13 @@
   :url "http://github.com/miner/wilkins"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
 
   ;; for Clojure master-SNAPSHOT releases
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
 
   :profiles {:clj15  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :clj14 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :rc1 {:dependencies [[org.clojure/clojure "1.6.0-RC1"]]}
              :snap {:dependencies [[org.clojure/tools.nrepl "0.2.3"]
-                                   [org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+                                   [org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              })
